03ef989dbe6b257bf5aa4d1259e17b52b010bbb2
Jonas Decraecker <jonas.decraecker@gmail.com>
2015-06-09 12:30:48 +0200

    Nieuw voorbeeld ButtonGebruik

---
+++ ButtonGebruik/ButtonGebruikWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ButtonGebruik
+{
+    /// <summary>
+    /// Interaction logic for ButtonGebruikWindow.xaml
+    /// </summary>
+    public partial class ButtonGebruikWindow : Window
+    {
+        public ButtonGebruikWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonBold_Checked(object sender, RoutedEventArgs e)
+        {
+            labelTekst.FontWeight = FontWeights.Bold;
+        }
+
+        private void buttonBold_Unchecked(object sender, RoutedEventArgs e)
+        {
+            labelTekst.FontWeight = FontWeights.Normal;
+        }
+
+        
+    }
+}
