f5eebcfa9a3f35c05bbb25424320700cca7dda93
Jonas Decraecker <jonas.decraecker@gmail.com>
2015-06-09 10:59:48 +0200

    Volledige oplossing van deze opgave

--- OpdrachtVerkeerslicht/Verkeerslicht.xaml.cs
+++ OpdrachtVerkeerslicht/Verkeerslicht.xaml.cs
@@ -24,5 +24,51 @@ namespace OpdrachtVerkeerslicht
         {
             InitializeComponent();
         }
+
+        private void veranderButton_Click(object sender, RoutedEventArgs e)
+        {
+            Button knop = (Button)sender;
+            SolidColorBrush kleur;
+            if (knop.Name == opgeletButton.Name)
+            {
+                if (this.roodLicht.Opacity == 1)
+                {
+                    this.roodLicht.Opacity = 0;
+                    this.goButton.IsEnabled = this.opgeletButton.IsEnabled;
+                    this.opgeletButton.IsEnabled = this.stopButton.IsEnabled;
+                    kleur = (SolidColorBrush)new BrushConverter().ConvertFromString(goButton.Tag.ToString());
+                    this.goButton.Background = kleur;
+                }
+                else
+                {
+                    this.groenLicht.Opacity = 0;
+                    this.stopButton.IsEnabled = this.opgeletButton.IsEnabled;
+                    this.opgeletButton.IsEnabled = this.goButton.IsEnabled;
+                    kleur = (SolidColorBrush)new BrushConverter().ConvertFromString(stopButton.Tag.ToString());
+                    this.stopButton.Background = kleur;
+                }
+                this.oranjeLicht.Opacity = 1;
+            }
+            else
+            {
+                if (knop.Name == goButton.Name)
+                {
+                    this.opgeletButton.IsEnabled = this.goButton.IsEnabled;
+                    this.goButton.IsEnabled = this.stopButton.IsEnabled;
+                    this.groenLicht.Opacity = this.oranjeLicht.Opacity;
+                }
+                else
+                {
+                    this.opgeletButton.IsEnabled = this.stopButton.IsEnabled;
+                    this.stopButton.IsEnabled = this.goButton.IsEnabled;
+                    this.roodLicht.Opacity = this.oranjeLicht.Opacity;
+                }
+                this.oranjeLicht.Opacity = 0;
+                kleur = (SolidColorBrush)new BrushConverter().ConvertFromString(opgeletButton.Tag.ToString());
+                this.opgeletButton.Background = kleur;
+            }
+
+        }
+
     }
 }
