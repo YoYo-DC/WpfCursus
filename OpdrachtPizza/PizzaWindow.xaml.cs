a8c9501a5d32a0b08c12dd0f4eb96e0c133b03f7
Jonas Decraecker <jonas.decraecker@gmail.com>
2015-06-10 08:59:18 +0200

    Einde pizza opdracht (maak opnieuw en hou rekening met suggesties in comment)

--- OpdrachtPizza/PizzaWindow.xaml.cs
+++ OpdrachtPizza/PizzaWindow.xaml.cs
@@ -47,12 +47,15 @@ namespace OpdrachtPizza
         }
 
         //Doorloop ieder child van grid. Als dit een stackpanel is, voeg dan de content van ieder ToggleButton- en Label-element in stringvorm toe aan een verzameling.
+        //Suggestie in dit geval: maak de eindboodschap aan het begin aan en vervolledig hem terwijl de foreach loop de child elementen doorloopt.
+        //Maak het code-behind gedeelte nog eens opnieuw en probeer rekening te houden met de suggestie.
         private void buttonBestellen_Click(object sender, RoutedEventArgs e)
         {
-            List<string> bestellingGegevens = new List<string>();
             ToggleButton knop;
             StackPanel paneel;
-            Label label;
+            List<string> ingredienten = new List<string>();
+            string grootte = string.Empty;
+            List<string> extras = new List<string>();
             foreach (FrameworkElement vak in gridBestelling.Children)
             {
                 if (vak is StackPanel)
@@ -60,21 +63,38 @@ namespace OpdrachtPizza
                     paneel = (StackPanel)vak;
                     foreach (FrameworkElement kind in paneel.Children)
                     {
-                        if (kind is ToggleButton)
+                        if (kind is RadioButton)
+                        {
+                            knop = (RadioButton)kind;
+                            if (knop.IsChecked == true)
+                                grootte = knop.Content.ToString();
+                        }
+
+                        else if (kind is CheckBox)
+                        {
+                            knop = (CheckBox)kind;
+                            if (knop.IsChecked == true)
+                                ingredienten.Add(knop.Content.ToString());
+                        }
+
+                        else if (kind is ToggleButton)
                         {
                             knop = (ToggleButton)kind;
                             if (knop.IsChecked == true)
-                                bestellingGegevens.Add(knop.Content.ToString());
-                        }
-                        if (kind is Label)
-                        {
-                            label = (Label)kind;
-                            bestellingGegevens.Add(label.Content.ToString());
+                                extras.Add(knop.Content.ToString());
                         }
                     }
                 }
             }
-            labelBestelling.Content = string.Format("U heeft {0} medium pizza('s) besteld met: {1}, {2} en {3} overstrooid met {4}",labelAantal.ToString());
+            string ingredientenLijn = string.Empty;
+            foreach (string woord in ingredienten)
+            {
+                if (ingredienten.IndexOf(woord) == ingredienten.Count-1)
+                    ingredientenLijn += "en ";
+                ingredientenLijn += woord+" ";
+
+            }
+            textBlockBestelling.Text = string.Format("U heeft {0} {1} pizza('s) besteld met: {2}",labelAantal.Content.ToString(),grootte,ingredientenLijn);
         }
     }
 }
