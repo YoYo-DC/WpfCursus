c4b2cc14d0b0432ce33ba8de0b8f6df56fa2a9da
Jonas Decraecker <jonas.decraecker@gmail.com>
2015-06-11 15:55:06 +0200

    OpdrachtTelefoon extra

---
+++ OpdrachtTelefoonTest/PersoonTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace OpdrachtTelefoonTest
+{
+    [TestClass]
+    public class PersoonTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
