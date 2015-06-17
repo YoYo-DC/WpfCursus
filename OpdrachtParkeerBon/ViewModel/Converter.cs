96afb2abde3085b38aa0ae512de96172ab1a3757
Jonas Decraecker <jonas.decraecker@gmail.com>
2015-06-17 14:19:14 +0200

    Nieuwe opdracht ParkingBonMetMVVM

---
+++ OpdrachtParkeerBon/ViewModel/Converter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace ParkingBonMVVM.ViewModel
+{
+    public class AmountToBoolean : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if ((string)value == "0 €")
+                return false;
+            return true;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
